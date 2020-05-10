d580b464b15f6e30c152d264c2e1b431cdf00cb3
Tha Gidi <you@example.com>
2020-05-10 16:35:53 +0100

    navigation modified to slide down

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
     // toggle mobile nav menu
     $('.menu-toggle').click(function(){
-        $('.responsive-nav').toggleClass("unfold animated slideInDown");
+        $('.responsive-nav').toggleClass("unfold");
         $(this).toggleClass("close-menu");
-    });
-
-    $('#responsive-nav, .unfold.nav-links').click(function(){
-        $('.responsive-nav').toggleClass("unfold animated slideInDown");
-        $('.menu-toggle').toggleClass("close-menu");
     });
 }); 
