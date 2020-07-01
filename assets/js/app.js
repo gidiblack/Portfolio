bcaebe9724babf3c828783797bd8ff3f25d58022
Tha Gidi <you@example.com>
2020-07-01 03:56:25 +0100

    page mods

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,23 @@
+// scroll to top
+var myBtn = document.getElementById('myBtn');
+window.onscroll = function (){
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+function topFunction (){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 $(document).ready(function(){
+    AOS.init(); // initialize aos animations
     // toggle mobile nav menu
     $('.menu-toggle').click(function(){
         $('.responsive-nav').toggleClass("unfold");
