0c0d29178b78ce2e80cfd47a07135a593ab45597
Gideon Etim <gidiblack@gmail.com>
2021-05-28 14:56:20 +0100

    formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
 // scroll to top
-var myBtn = document.getElementById('myBtn');
-window.onscroll = function (){
-    scrollFunction()
+var myBtn = document.getElementById("myBtn");
+window.onscroll = function () {
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        myBtn.style.display = "block";
-    } else {
-        myBtn.style.display = "none";
-    }
+  if (document.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
 }
 
-function topFunction (){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-$(document).ready(function(){
-    // toggle mobile nav menu
-    $('.menu-toggle').click(function(){
-        $('.responsive-nav').toggleClass("unfold");
-        $(this).toggleClass("close-menu");
-    });
-}); 
+$(document).ready(function () {
+  // toggle mobile nav menu
+  $(".menu-toggle").click(function () {
+    $(".responsive-nav").toggleClass("unfold");
+    $(this).toggleClass("close-menu");
+  });
+});
